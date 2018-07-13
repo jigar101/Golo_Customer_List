21eaf2c1b6b23553fba36cd012b891106e01a8e6
Jigar Thakkar <jigar9099@gmail.com>
2018-07-13 03:14:58 -0400

    no message

--- app/src/main/java/com/example/jigar/customerlist/MainActivity.java
+++ app/src/main/java/com/example/jigar/customerlist/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.jigar.customerlist;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RecyclerView recyclerView;
+    private ArrayList<Customer> data;
+    private CustomerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadData ();
+
+    }
+    public void loadData()
+    {
+        recyclerView = (RecyclerView)findViewById(R.id.rvList);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        data = new ArrayList<>(Arrays.asList(ApiCaller.getCustomers()));
+        adapter = new CustomerAdapter(data);
+        recyclerView.setAdapter(adapter);
     }
 }
